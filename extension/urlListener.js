79178b6f51e021156ba5c69a535759f712fc7bd3
Shannon Burns <shannon@codeshannon.com>
2014-03-14 15:32:43 -0700

    Refactoring, shows previous code and recreated after testing.

--- extension/urlListener.js
+++ extension/urlListener.js
@@ -14,39 +14,7 @@ function isUrlGmail(tabId, changeInfo, tab){
 // listen for url changes on any tab.
 chrome.tabs.onUpdated.addListener(isUrlGmail);
 
-
-// this is the call back function for the onClicked event
-// it tells you that your event listener is working. 
-// perhaps this is where I can add functionality later
-function onClickHandler(info, tab) {
-    if(info.menuItemId == "analyze text" || info.menuItemId == "generate meme response"){
-        console.log("You have selected the " + info.menuItemId + " context menu item");
-    } else {
-        console.log("item " + info.menuItemId + " was clicked");
-        console.log("info: " + JSON.stringify(info));
-        console.log("tab is: " + JSON.stringify(tab));
-    }
-};
-
-chrome.contextMenu.onClicked.addListener(onClickHandler);
-
-// set up the context menu tree when installed
-chrome.runtime.onInstalled.addListener(function() {
-    //create a context menu item
-    var seletionContext = ["selection"];
-    var title = "Analyze this " + seletionContext + " of text"
-    var id = chrome.contextMenus.create({"title": title, "contexts":[selectionContext],
-                                         "id": "context" + selectionContext});
-    console.log("I am logging because my context listener is working!");
-
-    //create a parent context menu item.
-    chrome.contextMenus.create({"title": "Test"})
-})
-
-
-//make a function to collect the selected text
-// try something like this (tested and works in console)
-// The onClicked callback function:
+//this is the refactored version of the thing written below.
 function onClickHandler(info, tab) {
   document.getElementsByTagName('body')[0].innerHTML+= info.selectionText;
   console.log(info.selectionText)
@@ -64,20 +32,48 @@ chrome.runtime.onInstalled.addListener(function() {
                                         "contexts": [contexts[1]],"id": "parent"});
 
 });
+// this is the call back function for the onClicked event
+// it tells you that your event listener is working. 
+// This is the thing I tried to build before I knew what things did
+            // function onClickHandler(info, tab) {
+            //     if(info.menuItemId == "analyze text" || info.menuItemId == "generate meme response"){
+            //         console.log("You have selected the " + info.menuItemId + " context menu item");
+            //     } else {
+            //         console.log("item " + info.menuItemId + " was clicked");
+            //         console.log("info: " + JSON.stringify(info));
+            //         console.log("tab is: " + JSON.stringify(tab));
+            //     }
+            // };
+
+            // chrome.contextMenu.onClicked.addListener(onClickHandler);
+
+            // // set up the context menu tree when installed
+            // chrome.runtime.onInstalled.addListener(function() {
+            //     //create a context menu item
+            //     var seletionContext = ["selection"];
+            //     var title = "Analyze this " + seletionContext + " of text"
+            //     var id = chrome.contextMenus.create({"title": title, "contexts":[selectionContext],
+            //                                          "id": "context" + selectionContext});
+            //     console.log("I am logging because my context listener is working!");
+
+            //     //create a parent context menu item.
+            //     chrome.contextMenus.create({"title": "Test"})
+            // })
+
 
 //TODO: either rebuild this or get it working better. it works in the console, 
 //but not in the extension. See if it's related to background page vs content script
 
-// function isReplyBoxOpen(){
-//     var pants = $('.editable');
-//     console.log(pants);
-//     if ($('.editable').length > 1){
-//         console.log("Yep, your reply box is open.");
-//         pants.innerHTML = '<img onload="src="https://images.nga.gov/?service=asset&action=show_preview&asset=19750">'
+            // function isReplyBoxOpen(){
+            //     var pants = $('.editable');
+            //     console.log(pants);
+            //     if ($('.editable').length > 1){
+            //         console.log("Yep, your reply box is open.");
+            //         pants.innerHTML = '<img onload="src="https://images.nga.gov/?service=asset&action=show_preview&asset=19750">'
 
-//     }else{
-//         console.log("nope, not working.")
-//     }
-// };
-// chrome.tabs.onClicked.addListener(isReplyBoxOpen);
+            //     }else{
+            //         console.log("nope, not working.")
+            //     }
+            // };
+            // chrome.tabs.onClicked.addListener(isReplyBoxOpen);
 
